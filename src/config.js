be4c4f2f9125099f5d26d31dac15f3ba0f41229e
Home <home@iMac-khxng-Home.local>
2020-03-27 20:08:45 +0700

    bug deploy

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBrCftH0OX1dg1up7vQ5RjkN60xd-KkXlc",
+    authDomain: "cswf-c8291.firebaseapp.com",
+    databaseURL: "https://cswf-c8291.firebaseio.com",
+    projectId: "cswf-c8291",
+    storageBucket: "cswf-c8291.appspot.com",
+    messagingSenderId: "344093463637",
+    appId: "1:344093463637:web:a0643578d21939dacd70dc",
+    measurementId: "G-JCCL3W6WQB"
+};
+
+  export default config;
